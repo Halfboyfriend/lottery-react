fbc156a7d6cb924dd079ac3e19acf4243dcf64af
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-09-03 10:09:49 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import './App.css';
 import LotteryApp from './components/LotteryApp';
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
  
   return (
     <div className='App'>
-      <LotteryApp/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<LotteryApp />} />
+          {/* <Route exact path='/tx' element={<TxDetail />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
